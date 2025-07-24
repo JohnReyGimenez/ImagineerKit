548e5a85556b295b5d5b28ace8aa8f48af9d1a02
JohnReyGimenez <gimenezjr5@gmail.com>
2025-07-24 21:06:13 +0800

    created browser router

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,19 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import 'index.css';
-import App from "App.jsx";
-import { createBrowserRouter } from "react-router-dom";
+import App from "/App";
+import Dashboard from "/Dashboard";
+import About from "/About";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+    { path:"/", element: <App/>},
+    { path:"/dashboard", element: <Dashboard/>},
+    { path:"/about", element: <About/>}
+]);
+
+createRoot(document.getElementById("root")).render(
+    <StrictMode>
+       <RouterProvider router={(router)}/>
+    </StrictMode>
+)
