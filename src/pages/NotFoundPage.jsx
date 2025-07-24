37ac5bf1b713705df056119d0d36f0d2787219ca
JohnReyGimenez <gimenezjr5@gmail.com>
2025-07-24 20:27:26 +0800

    added components and notfoundpage

---
+++ src/pages/NotFoundPage.jsx
@@ -0,0 +1,10 @@
+const NotFoundPage = () => {
+    return (
+        <div>
+            <h2>Page not found!</h2>
+        <link to={'/'}>
+            <button>Go back home</button>
+        </link>
+        </div>
+    )
+}
